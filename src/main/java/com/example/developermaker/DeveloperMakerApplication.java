c411e42b537464b92259dd13251feeb482a073a0
seonggon do <mooh2jj@naver.com>
2024-02-18 00:50:21 +0900

    feat: 개발자 프로젝트 first commit!

---
+++ src/main/java/com/example/developermaker/DeveloperMakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.developermaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperMakerApplication.class, args);
+    }
+
+}
